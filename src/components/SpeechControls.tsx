6a5866f0793ef99b692eaa73fcd006b227d80900
Angelica Amaro <angelica_at1@tesch.edu.mx>
2026-09-15 14:23:10 -0600

    fix: evita duplicar el botón de detener al mostrar los selectores de voz/velocidad

--- src/components/SpeechControls.tsx
+++ src/components/SpeechControls.tsx
@@ -138,7 +138,7 @@ export default function SpeechControls({
         ))}
       </select>
 
-      <StopSpeechButton onStop={onStop} active={isSpeaking} />
+        {!hideStop && <StopSpeechButton onStop={onStop} active={isSpeaking} />}
     </div>
   );
 }
